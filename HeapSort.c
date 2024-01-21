83f3be9f30a515dd10d5670447ea09d00da65d6b
UshasriMysarla <154975229+UshasriMysarla@users.noreply.github.com>
2024-01-21 12:41:45 +0530

    Add files via upload

---
+++ HeapSort.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+void heapify(int a[], int n, int i)
+{
+ 	int largest = i;
+ 	int left = 2 * i + 1;
+ 	int right = 2 * i + 2;
+ 	if (left < n && a[left] > a[largest])
+ 	{
+ 		largest = left;
+ 	} 
+	if (right < n && a[right] > a[largest])
+	{
+		largest = right;
+	}
+	if (largest != i)
+ 	{
+ 		int temp = a[i];
+ 		a[i] = a[largest];
+ 		a[largest] = temp;
+ 		heapify(a, n, largest);
+ 	}
+}
+void heapSort(int a[], int n)
+{
+	int i;
+ 	for (i = n / 2 - 1; i >= 0; i--)
+ 	{
+ 		heapify(a, n, i);
+ 	}
+ 	for (i = n - 1; i >= 0; i--)
+ 	{
+ 		int temp = a[0];
+ 		a[0] = a[i];
+ 		a[i] = temp;
+ 		heapify(a, i, 0);
+	}
+}
+int main()
+{
+ 	int array[100], n, i;
+ 	printf("Enter the number of elements: ");
+ 	scanf("%d", &n);
+ 	printf("Enter the elements: ");
+ 	for(i = 0; i < n; i++)
+ 	{
+ 		scanf("%d", &array[i]);
+ 	}
+ 	heapSort(array, n);
+ 	printf("Sorted araay: ");
+ 	for(i = 0; i < n; i++)
+ 	{
+ 		printf("%d ", array[i]);
+ 	}
+}
